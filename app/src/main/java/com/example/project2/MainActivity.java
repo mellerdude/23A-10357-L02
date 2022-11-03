0cdf306f0b8e66ceff70e37f0f8af619939601e0
mellerdude <omrimeller@gmail.com>
2022-11-03 11:11:47 +0200

    Init project

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//test
 public class MainActivity extends AppCompatActivity {
 
     @Override
